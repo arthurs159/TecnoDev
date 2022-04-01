c1a6e748c08d221acb90323beac04144287aad5d
Arthur Scarpati <arthur_159@hotmail.com>
2022-04-01 16:12:49 -0300

    Cleaning the project

--- src/main/java/br/com/tecnodev/controller/CategoryController.java
+++ src/main/java/br/com/tecnodev/controller/CategoryController.java
@@ -1,14 +1,8 @@
 package br.com.tecnodev.controller;
 
-import br.com.tecnodev.entities.category.CategoryActiveDto;
-import br.com.tecnodev.entities.category.CategoryProjection;
 import br.com.tecnodev.repository.CategoryRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 @RequestMapping("/category")
@@ -20,18 +14,4 @@ public class CategoryController {
         this.categoryRepository = categoryRepository;
     }
 
-
-//    @GetMapping("/list/active")
-//    @ResponseBody
-//    public List<CategoryActiveDto> listAllActiveCategoriesRepository() {
-//        List<CategoryActiveDto> categoryList = categoryRepository.findAllActiveUsers();
-//        return categoryList;
-//    }
-//
-//    @GetMapping("/api/categories")
-//    @ResponseBody
-//    public List<CategoryActiveDto> listAllActiveCategoriesProjection() {
-//        List<CategoryProjection> categoryList = categoryRepository.listProjection();
-//        return categoryList.stream().map(CategoryActiveDto::new).toList();
-//    }
 }
